42d9cfcd57db10efcd451b67844671f69a5400e1
DmetroSK <ygpramuditha@gmail.com>
2024-03-17 23:30:54 +0530

    Initial Commit

---
+++ BirthdayCountdownMenu/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BirthdayCountdownMenu
+//
+//  Created by Pramuditha Malhara on 2024-03-17.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
